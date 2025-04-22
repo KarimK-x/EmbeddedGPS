fd4c28029d5619e214c2a9d8dac8dc5b0fd219ef
marina-bebawy <marinabebawey12@gmail.com>
2025-04-23 00:48:46 +0200

    Updated GPIO.c

--- Drivers/INCLUDE/MCAL/BitMath.h
+++ Drivers/INCLUDE/MCAL/BitMath.h
@@ -9,10 +9,10 @@
 #ifndef BITMATH_H_
 #define BITMATH_H_
 
-#define SetBit(reg,pin) reg|=(1<<pin)
-#define ClrBit(reg,pin) reg&=(~(1<<pin))
-#define GetBit(reg,pin) (reg>>pin)&(1)
-#define TogBit(reg,bit) reg=reg^(1<<bit)
+#define SetBit(reg,pin) (reg|=(1<<pin))
+#define ClrBit(reg,pin) (reg&=(~(1<<pin)))
+#define GetBit(reg,pin) ((reg>>pin)&1)
+#define TogBit(reg,bit) (reg=reg^(1<<bit))
 
 
 #endif /* BITMATH_H_ */
