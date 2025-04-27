c1b3f44bd12329a1732355c1d681b98c5744ffce
Abanoub Osama <abanoubosama252@gmail.com>
2025-04-28 02:00:00 +0300

    systick

---
+++ Drivers/INCLUDE/MCAL/systick.h
@@ -0,0 +1,13 @@
+#include "tm4c123gh6pm.h"
+#define SYSTEM_CLOCK     16000000UL   // 16 MHz
+#define CYCLES_PER_MS    (SYSTEM_CLOCK / 1000)
+//CTRL_REG PINS
+
+
+
+void Systick_voidInit(void);
+void Systick_voidWait(int count);
+void Systick_voidDelayms(int time);
+void Systick_voidInitISR(void);
+void Systick_voidDisable(void);
+void SysTick_Handler(void);
